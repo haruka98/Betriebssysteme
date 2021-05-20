ec930121e87bc2f731e5310b127055642427ca84
Haruka <45176998+haruka98@users.noreply.github.com>
2021-05-20 09:32:57 +0200

    closed pipe

--- sheet2/pipe.c
+++ sheet2/pipe.c
@@ -17,11 +17,14 @@ int main(void) {
         return EXIT_FAILURE;
     case 0:
         dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
+        close(fd[1]);
         execlp("ls", "ls", NULL);
         perror("execlp(ls)");
         break;
     default:
         dup2(fd[0], STDIN_FILENO);
+        close(fd[0]);
         close(fd[1]);
         execlp("wc", "wc", "-l", NULL);
         perror("execl(ls)");
